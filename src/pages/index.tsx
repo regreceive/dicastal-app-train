d9d1c292ed389f54fc723e595fc8c49a169ab9d0
liyao <liyao@k2data.com.cn>
2021-03-29 16:26:16 +0800

    training-5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
-import { FC, useState } from 'react';
+import { FC, useCallback, useState } from 'react';
 import { Button } from 'antd';
 import styles from './index.less';
 
 const Main: FC = () => {
   const [count, setCount] = useState(0);
 
+  const add = useCallback(() => {
+    setCount((count) => count + 1);
+  }, []);
+
+  const subtract = useCallback(() => {
+    setCount((count) => count - 1);
+  }, []);
+
   return (
     <div className={styles.container}>
       <div>
-        <Button onClick={() => setCount((value) => value + 1)}>+</Button>
+        <Button onClick={add}>+</Button>
       </div>
       <div>{count}</div>
       <div>
-        <Button onClick={() => setCount((value) => value - 1)}>-</Button>
+        <Button onClick={subtract}>-</Button>
       </div>
     </div>
   );
