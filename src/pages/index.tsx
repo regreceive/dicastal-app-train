8aee306a349ae5e16c5ddb28346ae0e7dc2ff86b
liyao <liyao@k2data.com.cn>
2021-03-29 16:52:08 +0800

    training-6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
+
+function sleep() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(10);
+    }, 2000);
+  });
+}
 
 const Main: FC = () => {
-  return <div>Hello World!</div>;
+  const [count, setCount] = useState(0);
+
+  // useEffect(() => {
+  //   sleep().then((result: number) => {
+  //     setCount(result);
+  //   });
+  //   console.log("1222222");
+  // }, []);
+
+  const run = useCallback(async () => {
+    const count = (await sleep()) as number;
+    setCount(count);
+    console.log(1111111111111);
+  }, []);
+
+  useEffect(() => {
+    run();
+  }, []);
+
+  return <div>{count}</div>;
 };
 
 export default Main;
