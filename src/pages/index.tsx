f9146b175f41f9ac781be3e7c7fdb518fb75de30
liyao <liyao@k2data.com.cn>
2021-03-29 16:12:47 +0800

    training-4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,45 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { Button } from 'antd';
+import { useClickAway } from 'ahooks';
 
 const Main: FC = () => {
-  return <div>Hello World!</div>;
+  const [click, setClick] = useState(false);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (click) {
+      log('单击了');
+    }
+  }, [click]);
+
+  const log = useCallback(
+    (text: string) => {
+      console.log(text);
+    },
+    [click],
+  );
+
+  const msg = useMemo(() => {
+    return 'msg: ' + click.toString();
+  }, [click]);
+
+  useEffect(() => {
+    console.log(ref.current);
+  }, []);
+
+  return (
+    <div>
+      <Button
+        onClick={() => setClick(true)}
+        type={click ? 'primary' : 'default'}
+      >
+        按钮
+      </Button>
+      <div>{click.toString()}</div>
+      <div ref={ref}>{msg}</div>
+    </div>
+  );
 };
 
 export default Main;
